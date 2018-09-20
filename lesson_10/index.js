5bf77d555b4b13c2c0d26fb279084505dacff6e3
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-09-20 19:31:18 +0300

    Update index.js

--- lesson_10/index.js
+++ lesson_10/index.js
@@ -4,5 +4,6 @@ let ans1 = Math.max(20, 10, 11, 0, -2);
 let ans2 = Math.min(20, 10, 11,  0, -2);
 const average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;   
 const res = average( [ 20, 10, 11, 0, -2] );   
-console.log('max:' + ans1,'min:'+ ans2,'avg:'+ res);
+console.log(obj);
+const  obj = {max:ans1, min:ans2, avg:res};
 
