8f92806e9f1af4a790ce9479f478530a46931d91
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-09-20 17:16:16 +0300

    Update index.js

--- lesson_10/index.js
+++ lesson_10/index.js
@@ -1,6 +1,8 @@
-const a = 50,  b =100 ;
-console.log(b);
-console.log(a);
-console.log(a+b);
-console.log(a/b);
-console.log(a+10>=b ? a+10:9);
+'use strict'
+const arr1  = [20, 10, 11, 0, -2];
+let ans1 = Math.max(20, 10, 11, 0, -2);
+let ans2 = Math.min(20, 10, 11,  0, -2);
+const average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;   
+const res = average( [ 20, 10, 11, 0, -2] );   
+console.log('max:' + ans1,'min:'+ ans2,'avg:'+ res);
+
