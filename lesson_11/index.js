5566931ee1fea3120e81a706e30ab224d52eb706
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-09-24 17:39:35 +0300

    Update index.js

--- lesson_11/index.js
+++ lesson_11/index.js
@@ -1,1 +1,33 @@
+'use strict'
+const object = {
+	  a:'1',
+    b:'2', 
+    c:'3', 
+    d:'4',
+    e:'5'};
+const word = [ 'b', 'c', 'd'];
+function removeKeys1(object, word){
+  for (let i in object1){
+    let k = word.includes(i);
+    if(k){
+      delete object[i];
+    }
+  }
+   	return object1;
+   };
+
+
+const object1 = {};
+function removeKeys2(object1, word){
+	return  object1 = removeKeys1( word);
+   }
+console.log(removeKeys2(object1,word))
+
+let a = 3;
+let b = 7;
+function absDiff(a) {
+	function abs2(b){
+		console.log(abs2())
+	}	return b - a
+	}
 
