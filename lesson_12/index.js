6c5fa3e86ad4fd9c1b59d1181dbe1a79dbf05e33
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-09-27 18:19:36 +0300

    Update index.js

--- lesson_12/index.js
+++ lesson_12/index.js
@@ -1,1 +1,25 @@
+'use strict'
+const arr = [[1, null, 3], [7, '', 9]
+function clearNumbers(arr){
+  const arr = [[1, null, 3], [7, '', 9];
+  function filter(arr) => {typeof arr === 'number'}
+    return arr
+};
 
+
+function reverse(a, b){
+  let a = ('123');
+  let b = ('456');
+  {}
+  return
+
+let arr = [1, 2, 3, 4, 5, 6];
+let n = 3;
+function clearNumbers(arr, n) {
+  var res = [];
+  while (arr.length) {
+    res.push(arr.splice(0, n));
+  }
+  return res;
+};
+console.log(clearNumbers(arr, n));
