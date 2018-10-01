afaac75cc6e671cb0d4cd67ec32d1e16eb5dd0a1
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-10-01 17:26:43 +0300

    Add files via upload

--- lesson_12./index.js
+++ lesson_12./index.js
@@ -1,1 +1,36 @@
+'use strict'
+function add(string) {
+  if( string  === underfined){
+  string = ''
+  }
 
+};
+const arr = [[1, '1', 3],[ 7, 'top', 9]];
+ function ClearNumber(arr){
+  let arr1 = function filter(arr){typeof arr === 'number'};
+};
+
+let a = ('123');
+let b = ('456');
+let reverse = function (a) {
+let n = a.toString();
+let rev = [];
+for (let i = n.length-1; i >=0; i--){
+rev+=n[i]
+}
+return parseInt(rev)
+
+}
+
+console.log(a);
+
+let arr = [1, 2, 3, 4, 5, 6];
+let n = 3;
+function clearNumbers(arr, n) {
+  const res = [];
+  for (let i = 0; i < arr.length; i += n) {
+    res.push(arr.slice(i, i + n));
+  }
+  return res;
+};
+console.log(clearNumbers(arr, n));
