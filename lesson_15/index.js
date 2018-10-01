8f3ae108aa6f3c5153c064cf90949711ebe7528d
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-10-01 16:16:53 +0300

    Add files via upload

--- lesson_15/index.js
+++ lesson_15/index.js
@@ -1,1 +1,32 @@
+'use strict'
+const object = {
+	  a:'1',
+    b:'2', 
+    c:'3', 
+    d:'4',
+    e:'5'};
+const word = [ 'b', 'c', 'd'];
+function removeKeys1(object, word){
+  for (let i in object){
+    let k = word.includes(i);
+    if(k){
+      delete object[i];
+    }
+  }
+   	return object;
+   };
 
+
+const object1 = {};
+function removeKeys2(object1, word){
+	return  object1 = removeKeys1(object, word);
+   }
+
+let a = 3;
+let b = 7;
+function absDiff(a) {
+	function abs2(b){
+		console.log(abs2())
+	}	return b - a
+	}
+  console.log(absDiff(a));
