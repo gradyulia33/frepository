dec1b1ea032c14d0f95eddbc0dddf55e40ee5a4a
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-10-04 18:27:45 +0300

    Add files via upload

--- lesson_14/index.js
+++ lesson_14/index.js
@@ -1,1 +1,20 @@
-
+'use strict'
+const arg = {className:'class', attributes:{'data - name': 'Anatoliy'}, content:'Some Text'};
+const n;
+function init(arg, n) {
+	const ul = document.createElement(ul).body;
+  for (let i = 0; i < arg.length; i+=1) {
+    //для каждого елемента в массиве создать
+  const n = document.createElement(li).ul;
+  }
+  
+}
+console.log(init(arg, n));
+function listen() {
+  const button = document.addEventListener('button', () => console.log('clicked');
+   if (button = delete(ul)){
+    return true};
+   if (button != delete(ul)){
+   return false};
+}
+console.log()
