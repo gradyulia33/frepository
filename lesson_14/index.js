a19f2d1268c33ffab7ce0fdfe0154552897ba1bd
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-10-08 17:38:22 +0300

    Add files via upload

--- lesson_14/index.js
+++ lesson_14/index.js
@@ -1,28 +1,29 @@
 'use strict'
+
 function init(array, n) {
-	let newUl = document.createElement('ul');
-	document.body.appendChild(newUl);
-	let maxLength = (array.length > n ? n : array.length);
-	for (let i = 0; i < maxLength; i += 1) {
-		let newLi = document.createElement('li');
-		let classKey = array[i].className;
-		newLi.className += classKey;
-		let attrKey = Object.keys(array[i].attributes);
-		for (let j of attrKey) {
-			newLi.setAttribute(j, array[i].attributes[j]);
-		}
-		newLi.innerText = array[i].content;
-		newUl.appendChild(newLi);
-	}
+    let newUl = document.createElement('ul');
+    document.body.appendChild(newUl);
+    let maxLength = (array.length > n ? n : array.length);
+    for (let i = 0; i < maxLength; i += 1) {
+        let newLi = document.createElement('li');
+        let classKey = array[i].className;
+        newLi.className += classKey;
+        let attrKey = Object.keys(array[i].attributes);
+        for (let j of attrKey) {
+            newLi.setAttribute(j, array[i].attributes[j]);
+        }
+        newLi.innerText = array[i].content;
+        newUl.appendChild(newLi);
+    }
 }
 
 // init(arr, 4);
 
 
 function listen() {
-	let remUl = document.getElementsByTagName('ul')[0];
-	let button = document.getElementById('button');
-	button.addEventListener('click', () => 
-	remUl.parentElement.removeChild(remUl))
+    let remUl = document.getElementsByTagName('ul')[0];
+    let button = document.getElementById('button');
+    button.addEventListener('click', () => 
+    remUl.parentElement.removeChild(remUl))
 }
 console.log(listen());
