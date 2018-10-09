9237eca8f6dc0efe0dce63ed7d46f27f866dceec
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-10-09 21:58:15 +0300

    Add files via upload

--- module/index.js
+++ module/index.js
@@ -1,1 +1,80 @@
+'use strict'
 
+//Задача 1
+const a = 'mario';
+function itsMe(a) {
+	return ("It's me," + " " + a);
+};
+
+//Задача 2
+const b = 5 ;
+const c = 2;
+function compare(b, c) {
+if( b > c ){
+   return (b + '>' + c);
+} else if (b < c){
+   return (b + '<' + c);
+} else if ( b == c ){
+	return (b + '==' + c);
+} else ( typeof b || typeof c !== 'number')
+	return ('НЕ ЧИСЛО');	
+};
+
+//Задача 3
+const f = 3;
+const g = -4;
+const h = 5;
+const j = '>';
+function row(f, g, h, j) {
+	const arg = [];
+	for (let i = 0; i < row.length; i++) { 
+	//	 arg.sort(i);
+	}
+	return 
+};
+
+//Задача 4
+const n = 4;
+function fact(n) {
+	return n ? n * fact(n - 1): 1;
+}
+//Задача 5
+const arg1 = [[2, 3], [4, 5]];
+const arg2 = [[2, 1], [6, 5]];
+function matrixDiff(arg1, arg2) {
+	for (let i = 0; i < arg1.length; i+= 1) {
+		for (let j = 0; j < arg2.length; i+=1) {
+			arg[j] = i
+		}
+		
+		sum+= arg[i];
+	}
+};
+
+//Задача 6
+const arr = ['Bill', 'Kill', 'Song'];
+function strangeSearch(arr) {	
+const goBtn = document.getElementbyId('go');
+      goBtn.addEventListener('click', () => {
+	  window.location.href= "https://www.youtube.com/?gl=UA&hl=ru" + (input text value);
+	});
+        const result = [];
+		for (let i = 0; i < arr; i += 1) {
+			const j = document.createElement('input');
+			j.innerText = i + 1;
+			arr.appendChild(result);
+			result.push(j);
+			
+		return result;
+}
+const bill = document.createElement('input');
+	bill.type = 'number';
+    bill.name = 'Bill';
+const text = document.createElement('input');
+	text.type = 'number';
+	text.name = 'Kill';
+const song = document.createElement('input');
+	song.type = 'number';
+	song.name = 'Song';
+
+};
