21d45bc433d27b15dadac4c3d5d320393e4950f8
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-10-15 20:37:12 +0300

    Add files via upload

--- lesszon_17/index.js
+++ lesszon_17/index.js
@@ -1,1 +1,21 @@
+'use strict'
+const date = document.getElementById('date');
+const input = document.getElementById('input');
+const res = document.getElementById('res');
 
+function dates(res, input){
+input = new Date("21-10-2018");
+let newdate = input.toGMTString();
+let nowdate = Date.now();
+let result = newdate - nowdate;
+result /= 1000;
+result /=60;
+result /=60;
+result /=24;
+//let hours = date.getHours();
+//date.toISOString();
+//result.toGMTString());
+alert(result);
+};
+
+    
