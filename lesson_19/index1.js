4a2e60320e64eda9f9070a68d42aef43b0db00f7
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-10-23 00:15:20 +0300

    Add files via upload

---
+++ lesson_19/index1.js
@@ -0,0 +1,21 @@
+'use strict'
+Human  = {name ='Yulia', years = 20};
+function Human(name, years){
+            this.name,
+            this.years,
+}
+Human.prototype.sayHello = function(){ console.log( `Hello, my name is  ${this.name}, i am ${this.years} years old` ) };
+function AlevelStudent(name, years,marks){
+    Human.call(this, name, years);
+    this.marks = marks;
+}
+AlevelStudent.prototype = Object.create(Human.prototype);
+AlevelStudent.prototype.averageMark = function(){
+    let sum = 0;
+    for(let i in this.marks) {
+        sum += this.marks[i];
+    }
+    let result = sum / this.marks.length;
+    return result;
+    console.log(result);
+}
