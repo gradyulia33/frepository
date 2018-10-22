26be308821e7b1257cec1a429db2c0e254678c0d
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-10-23 00:33:16 +0300

    Update index.js

--- lesson_18/index.js
+++ lesson_18/index.js
@@ -1,45 +1,17 @@
 'use strict'
-const object = {
-	  a:'1',
-    b:'2', 
-    c:'3', 
-    d:'4',
-    e:'5'};
-const word = [ 'b', 'c', 'd'];
-function removeKeys1(object, word){
-  for (let i in object){
-    let k = word.includes(i);
-    if(k){
-      delete object[i];
-    }
-  }
-   	return object;
-   };
-
-function removeKeys2(object1, word){
-  const object1 = {};
-  const keys = Object.keys(object);
-  for (let i = 0; i < keys.length; i+=1) {
-   const key = keys[i];
-   let exist = false;
-
-   for (let j = 0; j < word.length; j+=1) {
-     if (key ===word[j])}{
-      exist = true;
-      break;
-     }
-   }
-  if (!exist){
-    object1{key} = object{key};
-  }
-	return  object1;
-   }
-
-let a = 3;
-let b = 7;
-function absDiff(a) {
-	function abs2(b){
-		return  Math.abs(b - a);
-	}
-  return abs2
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({isAuth:true})
+    },2000);
+})
+.then((authInfo)=>new Promise(function(resolve,reject){
+    if (!authInfo.isAuth) {
+        reject(null)
+    }else{
+    setTimeout(function(){
+        resolve({name:"Max"})
+    },2000);
 }
+}))
+.then((user)=>console.log(user.name))
+.catch((err)=>err);
