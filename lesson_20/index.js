dd5a046aed557bff725bda752a081c78ffb19853
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-10-27 02:15:07 +0300

    Add files via upload

--- lesson_20/index.js
+++ lesson_20/index.js
@@ -1,1 +1,11 @@
+'use strict'
+/*Структура проекта:
+index.js:
+    В этои файле должен происходить import классов LocationApi и Dom и работа с ними
+
+Задача: взять ваши координаты локации и положить их в dom елемент.*/
+//cathe сделать запрос на айпи
+//каждый метод отдельно
+import LocationApi from './location_api.js';
+import Dom from './dom.js';
 
