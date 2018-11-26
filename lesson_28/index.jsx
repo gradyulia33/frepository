ed698a008319c2385129d1b4aadb64f84a43b549
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-11-27 00:01:30 +0200

    Update index.jsx

--- lesson_28/index.jsx
+++ lesson_28/index.jsx
@@ -1,1 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Link, Switch, Redirect} from 'react-router-dom';
+
+import Main from './main';
+import About from './about';
+import NotFound from './not_found';
+
+class Result extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <Router>
+        <React.Fragment>
+          <Switch>
+            <Route path="/" exact component={Main} />
+            <Route path="/about" exact component={About} />
+            <Route path="/not_found" component={NotFound} />
+            <Redirect to='/not_found' />
+          </Switch>
+        </React.Fragment>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<HiLinks />, document.getElementById('root'));
 
