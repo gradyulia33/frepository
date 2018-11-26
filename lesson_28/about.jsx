c08f0cfc32f9b24bfc25c5c693ef75df5c20bed1
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-11-26 23:59:59 +0200

    Create about.jsx

---
+++ lesson_28/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class About extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+		return(
+			<h1>About component</h1>
+		)
+	}
+}
