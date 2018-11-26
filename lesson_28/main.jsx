6d2ddfd7b763bd540aab134457038e2f2ca5d51c
gradyulia33 <42874264+gradyulia33@users.noreply.github.com>
2018-11-27 00:03:58 +0200

    Create main.jsx

---
+++ lesson_28/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+		return(
+			<h1>Main component</h1>
+		);
+	}
+}
